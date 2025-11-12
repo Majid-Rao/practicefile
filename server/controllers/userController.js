e03ea3f88a9de2a78d4ea22f4a771ff96dd24501
Majid-Rao <96466989+Majid-Rao@users.noreply.github.com>
2025-11-12 15:59:20 +0500

    fixes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,10 +2,27 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const userRegister = asyncHandler(async(req,res)=>{
-   res.status(200).json({
-    message:"ok",
-   })
+   // res.status(200).json({
+   //  message:"ok",
+   // })
+   
 })
 
 export {
     userRegister};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,3 +7,4 @@ router.route('/register').post(userRegister);
 
 
 export default router;
+
